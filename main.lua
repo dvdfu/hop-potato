73156bde52cabd8806fbd1279d577e25f393ed47
Andrew Park <andrew@andrews-mbp.corp.kik.com>
2015-04-06 16:08:27 -0400

    Added timer

--- main.lua
+++ main.lua
@@ -9,13 +9,25 @@ function love.load()
 	for i = 0, 10, 1 do
 		platforms[i] = Platform:new(20 * i, 30 * i, 7 * i)
 	end
+
+	font = love.graphics.newFont(14)
+	love.graphics.setFont(font)
+
+	timer = 5
 end
 
 function love.update(dt)
+	if timer <= 0 then 
+		timer = 0
+	else 
+		timer = timer - dt
+	end
 end
 
 function love.draw()
 	for i = 0, 10, 1 do
 		platforms[i]:draw()
 	end
+
+	love.graphics.printf(string.format("%.1f", timer), love.graphics.getWidth() - 34, 10, 25, 'right')
 end
