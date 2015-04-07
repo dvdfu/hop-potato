95d8b6232d1e32b7a997671462db630e3e2116bc
david <davidf1212@gmail.com>
2015-04-07 11:15:17 -0400

    small update

--- platform.lua
+++ platform.lua
@@ -13,6 +13,10 @@ function Platform:initialize()
 	world:add(self, self.x, self.y, self.w, 16)
 end
 
+function Platform:update(dt)
+	world:update(self, self.x, self.y)
+end
+
 function Platform:draw()
 	love.graphics.rectangle('fill', self.x, self.y, self.w, 16)
 end
@@ -20,12 +24,10 @@ end
 function Platform:move()
 	local destX = math.random(0, love.graphics.getWidth() - self.w)
 	local destY = math.random(100, love.graphics.getHeight() - 16)
-	local actualX, actualY, cols, len = world:move(self, destX, destY, type)
-	-- self.x, self.y = actualX, actualY
-	Flux.to(self, 0.3, {
-		x = actualX,
-		y = actualY
-	})
+	Flux.to(self, 1, {
+		x = destX,
+		y = destY
+	}):ease('cubicout')
 end
 
 return Platform
