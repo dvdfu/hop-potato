e8ee807578ce42facc1c0fddcae30f3a6a9a3994
David <davidf1212@gmail.com>
2015-04-07 18:12:49 -0400

    lava

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 Class = require 'lib.middleclass'
+PlayScreen = require 'play-screen'
 MenuScreen = require 'menu-screen'
 
 screens = {}
@@ -6,12 +7,19 @@ screens = {}
 function love.load()
 	math.randomseed(os.time())
 	love.graphics.setDefaultFilter('nearest', 'nearest')
-	love.graphics.setBackgroundColor(40, 50, 60)
+	love.graphics.setBackgroundColor(40, 40, 40)
+
+	font = love.graphics.newImageFont("img/font.png", " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/():;%&`'*#=[]\"")
+	love.graphics.setFont(font)
+
 	screenNum = 0
-	screens:enterScreen(MenuScreen)
+	screens:enterScreen(PlayScreen)
 end
 
 function love.update(dt)
+	if love.keyboard.isDown('escape') then
+		love.event.push('quit')
+	end
 	screens[screenNum]:update(dt)
 end
 
