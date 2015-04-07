15f9126c44522f1dda469715c0b093f2e42d422e
Stefan Wojcik <ert.mcscrad@gmail.com>
2015-04-07 15:27:33 -0400

    title addition

--- menu-screen.lua
+++ menu-screen.lua
@@ -4,11 +4,14 @@ MenuScreen = Class('MenuScreen')
 
 function MenuScreen:initialize()
 	timer = 1
+
+	self.title = 'DON\'T STEP IN THE LAVA AND MAKE SURE TO GET RID OF THE POTATO'
 end
 
 function MenuScreen:update(dt)
 	if timer > 0 then
 		timer = timer - dt
+		if timer < 0 then timer = 0 end
 	else
 		screens:enterScreen(PlayScreen)
 		timer = 5
@@ -16,6 +19,7 @@ function MenuScreen:update(dt)
 end
 
 function MenuScreen:draw()
+	love.graphics.print(self.title, love.graphics.getWidth()/2-string.length(self.title)/2, love.graphics.getHeight()/3)
 	love.graphics.print(string.format('%.3f', timer), 300, 300)
 end
 
