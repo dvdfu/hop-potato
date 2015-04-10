046d844fb74ac4a0440cb9b2104846d4cf3bc7c6
David <davidf1212@gmail.com>
2015-04-10 11:07:04 -0400

    dashes

--- dictionary.lua
+++ dictionary.lua
@@ -13,7 +13,7 @@ end
 function Dictionary:generateName()
   local n1 = math.random(1, self.length)
   local n2 = math.random(1, self.length)
-  return self:firstToUpper(self.dict[n1]) .. self:firstToUpper(self.dict[n2])
+  return self:firstToUpper(self.dict[n1]) .. '-' .. self:firstToUpper(self.dict[n2])
 end
 
 function Dictionary:firstToUpper(str)
